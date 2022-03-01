5e6f667477d6a74acd3d46a1f80a17e975f07195
Seth Thomas <athomas.seth@gmail.com>
2022-03-01 14:48:14 -0500

    Created array and displayed list on page

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Hello Mars!");
+const roverList = document.getElementById("rover");
+
+const rovers = ["Sojourner", "Spirit", "Opportunity", "Curiosity", "Perseverance"];
+const addElement=(el)=> {for (i in rovers){
+    const li =document.createElement("li");
+    li.appendChild(document.createTextNode(rovers[i]));
+    el.appendChild(li);
+
+}}
+addElement(roverList);
